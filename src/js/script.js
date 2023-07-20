4b07b72b012acefc47410cd0daf7dfce6b6650ee
devCKVargas <50130192+devCKVargas@users.noreply.github.com>
2023-07-20 11:29:25 +0800

    To do: offset fixed navbar

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,48 @@
 const navMenu = document.querySelector(".nav__menu");
-const header = document.getElementsByTagName("header");
+const header = document.getElementsByTagName("header")[0];
+const heroArrow = document.querySelector(".hero__arrow");
 
 window.onscroll = () => {
 	if (window.scrollY > 100) {
-		header[0].classList.add("sticky");
-		header[0].style.setProperty("--selection-background", "var(--clr-accent)");
+		header.classList.add("sticky");
+		header.style.setProperty("--selection-background", "var(--clr-accent)");
 	} else {
-		header[0].classList.remove("sticky");
-		header[0].style.setProperty("--selection-background", "var(--clr-primary)");
+		header.classList.remove("sticky");
+		header.style.setProperty("--selection-background", "var(--clr-primary)");
 	}
 };
 
 navMenu.addEventListener("click", () => {
 	const navList = document.querySelector(".nav__list");
 	navList.classList.toggle("hidden");
+});
+
+// const heroArrow = document.querySelector(".hero__arrow");
+// const header = document.getElementsByTagName("header");
+// const navBarScrollFix = () => {
+// 	const navbarHeight = header.offsetHeight;
+// 	const productSection = document.querySelector("#product-section");
+// 	const section = productSection.getBoundingClientRect().top;
+
+// 	window.scrollBy({
+// 		top: section - navbarHeight, // Subtract navbar height as an offset
+// 	});
+// };
+
+console.log(header.offsetHeight);
+//
+
+const productSection = document.querySelector("#product-section");
+
+const navBarOffset = () => {
+	const headerHeight = header.getBoundingClientRect().height;
+	const elementPosition = productSection.getBoundingClientRect().top;
+	window.scrollBy({
+		top: elementPosition - headerHeight,
+		behavior: "smooth",
+	});
+};
+
+heroArrow.addEventListener("click", () => {
+	navBarOffset();
 });
